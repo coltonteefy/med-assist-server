93e7fdd152bc22a5ebee6ade4a761caf73c2b2bc
coltonteefy <coltonteefy@yahoo.com>
2018-09-12 16:03:18 -0700

    updating port and removing old tasks model

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-// var taskController = require('./controllers/tasks');
 var userController = require('./controllers/user');
 mongoose.connect('mongodb://admin:admin2018@ds251632.mlab.com:51632/musicondb', {useNewUrlParser: true});
 var app = express();
@@ -10,18 +9,14 @@ var router = express.Router();
 
 app.use('/api', router);
 
-app.get('/', function (req, res) {
-    res.send("Welcome to app")
-});
-
-// router.route('/allUsers').get(userController.getAllUsers);
-// router.route('/users/:_id').get(userController.getUserById);
-// router.route('/user/:_id').get(userController.getUser);
-// router.route('/addUser').post(userController.addUser);
-// router.route('/updateUser/:_id').post(userController.updateUser);
-// router.route('/deleteUser/:_id').get(userController.deleteUser);
+router.route('/allUsers').get(userController.getAllUsers);
+router.route('/users/:_id').get(userController.getUserById);
+router.route('/user/:_id').get(userController.getUser);
+router.route('/addUser').post(userController.addUser);
+router.route('/updateUser/:_id').post(userController.updateUser);
+router.route('/deleteUser/:_id').get(userController.deleteUser);
 
 
-app.listen(3000, function () {
-    console.log("Server listening on port 3000");
+app.listen(5000, function () {
+    console.log("Server listening on port 5000");
 });
