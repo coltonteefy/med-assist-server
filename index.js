93d3545d5029608e909030cce04af18e2cf4e4ee
coltonteefy <coltonteefy@yahoo.com>
2018-10-17 23:06:59 -0700

    add socket.io for chat

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ var router = express.Router();
 var userRoutes = require('./routes/user');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
 mongoose.set('useCreateIndex', true);
 mongoose.connect('mongodb://admin2018:admin2018@ds053459.mlab.com:53459/med-assist-users', {useNewUrlParser: true});
 
@@ -68,6 +74,51 @@ router.route('/getAllUsers').get(userRoutes.getAllUsers);
 router.route('/updateUser/:_id').post(userRoutes.updateUser);
 router.route('/deleteUser/:_id').get(userRoutes.deleteUser);
 
-app.listen(PORT, function () {
-    console.log("Server listening on port 5000");
+
+// app.listen(PORT, function () {
+//     console.log("Server listening on port 5000");
+// });
+
+http.listen(app.get('port'), function() {
+    console.log('Server listening on port', app.get('port'));
+});
+
+
+//IO CONNECTION
+io.on('connection', function(socket){
+
+    console.log('New Conncetion made');
+
+    socket.on('connect user', function(user){
+        console.log("Connected user ");
+        io.emit('connect user', user);
+    });
+
+    socket.on('on typing', function(typing){
+        console.log("Typing.... ");
+        io.emit('on typing', typing);
+    });
+
+    socket.on('chat message', function(msg){
+        console.log("Message " + msg['message']);
+        io.emit('chat message', msg);
+    });
+    
+//DIFFERENT HERE
+    socket.on('join', function(data){
+        //joining
+        socket.join(data.room);
+        console.log(data.user + 'joined the room : ' + data.room);
+        socket.broadcast.to(data.room).emit('new user joined', {user:data.user, message:'has joined this room.'});
+    });
+
+    socket.on('leave', function(data){
+        console.log(data.user + 'left the room : ' + data.room);
+        socket.broadcast.to(data.room).emit('left room', {user:data.user, message:'has left this room.'});
+        socket.leave(data.room);
+    });
+
+    socket.on('message',function(data){
+        io.in(data.room).emit('new message', {user:data.user, message:data.message});
+    })
 });
