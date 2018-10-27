d775eff40b8f8aefdd272a23ceddc821dd483c07
coltonteefy <coltonteefy@yahoo.com>
2018-10-27 12:38:09 -0700

    changes

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect('mongodb://admin2018:admin2018@ds053459.mlab.com:53459/med-assi
 app.set('view engine', 'ejs');
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
